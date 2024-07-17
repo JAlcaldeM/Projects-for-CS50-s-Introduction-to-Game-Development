c29badc6dca2926036e753ff7f9695e405aacce7
JAlcaldeM <javieralcaldemarchena3a@gmail.com>
2024-07-17 16:54:02 +0200

    8. Helicopter added

---
+++ 8. Helicopter/Assets/Resources/Scripts/CoinSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinSpawner : MonoBehaviour {
+
+    public GameObject coinPrefab;
+    public GameObject gemPrefab;
+    private float gemProbability = 0.2f;
+
+    void Start () {
+        StartCoroutine(SpawnItems());
+    }
+
+    void Update () {}
+
+    IEnumerator SpawnItems() {
+        while (true) {
+            int coinsThisRow = Random.Range(1, 4);
+
+            if (Random.value < gemProbability) {
+                Instantiate(gemPrefab, new Vector3(26, Random.Range(-6, 6), 10), Quaternion.identity);
+            } else {
+                for (int i = 0; i < coinsThisRow; i++) {
+                    Instantiate(coinPrefab, new Vector3(26, Random.Range(-6, 6), 10), Quaternion.identity);
+                }
+            }
+
+            yield return new WaitForSeconds(Random.Range(1, 5));
+        }
+    }
+}
